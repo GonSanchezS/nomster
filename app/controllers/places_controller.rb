227f1d07f74bae2ae25f13ec7e7903716565055c
Gonzalo <gonsanchezs@gmail.com>
2016-06-20 23:30:57 +0000

    Added edit user permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   
   def index
     #@places = Place.all #Display all entries in the Place model, and store it in @places.
@@ -25,12 +25,21 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'You can only edit places you\'ve created', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
     redirect_to place_path(@place)
+
+    if @place.user != current_user
+      return render text: 'You can only edit places you\'ve created', status: :forbidden
+    end
+
   end
 
   def destroy
