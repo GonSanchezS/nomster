6cf2a1f44847f77f4c03ac1aa0545a26609b0730
Gonzalo <gonsanchezs@gmail.com>
2016-06-21 11:24:51 +0000

    Added validation to name, place and address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true 
+  validates :name, length: { minimum: 3, 
+    too_short: "Name should have more than %{count} characters"}
 end
